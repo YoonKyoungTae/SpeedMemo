49668b69eeacfa3cb9e291255f7ff9449d371484
ryans <yoon91.dev@gmail.com>
2018-01-30 00:09:19 +0900

    리소스 수정

--- app/src/main/java/kr/co/diordna/speedmemo/adapter/MemoListAdapter.java
+++ app/src/main/java/kr/co/diordna/speedmemo/adapter/MemoListAdapter.java
@@ -30,7 +30,7 @@ public class MemoListAdapter extends RecyclerView.Adapter<MemoListAdapter.ViewHo
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_memo, parent, false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_memo_vertical, parent, false);
         return new ViewHolder(view);
     }
 
